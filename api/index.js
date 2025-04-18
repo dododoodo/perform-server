bea2580b11ba97e43c941a77b33a45347ab73e1d
Doyeon Kim <nsa10050@naver.com>
2025-04-18 17:35:56 +0900

    first commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const kakao = require('./kakao');
+const naver = require('./naver');
+const kcisa = require('./kcisa');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/kakao', kakao);
+app.use('/naver', naver);
+app.use('/kcisa', kcisa);
+
+module.exports = app;
