f596e01e5c4ae44e579abf93ccf9fc6aaeac27e1
Doyeon Kim <nsa10050@naver.com>
2025-04-18 18:06:28 +0900

    first commit

--- api/kakao.js
+++ api/kakao.js
@@ -1,11 +1,8 @@
-require('dotenv').config(); // .env 로딩
-
 const express = require('express');
 const kakao = express.Router();
 const axios = require('axios');
 const { MongoClient } = require('mongodb');
 
-// MongoDB 설정
 const uri = "mongodb+srv://nsa10050:rlaehdus0823@gotoashow.9ufcsbx.mongodb.net/?retryWrites=true&w=majority&appName=gotoashow";
 const client = new MongoClient(uri);
 
@@ -18,11 +15,12 @@ async function dataCtrl() {
     console.log("MongoDB 연결 완료");
 }
 
-initDB();
-
+// 서버 시작 시 데이터베이스 연결
 async function initDB() {
     await dataCtrl();
 }
+
+initDB();
 
 kakao.get('/', async function (req, res) {
     const { code } = req.query;
@@ -32,20 +30,21 @@ kakao.get('/', async function (req, res) {
     }
 
     try {
-        const clientId = process.env.KAKAO_CLIENT_ID;
-        const redirectUri = process.env.CLIENT_REDIRECT_URI;
-
         let tokenResponse = await axios.post("https://kauth.kakao.com/oauth/token", null, {
             headers: { "Content-Type": "application/x-www-form-urlencoded;charset=utf-8" },
             params: {
                 grant_type: "authorization_code",
-                client_id: clientId,
-                redirect_uri: redirectUri,
+                client_id: "f26d70de4f91fb13430539fe82bcebfc",
+                redirect_uri: CLIENT_REDIRECT_URI,
                 code
             }
+            
         });
 
-        const access_token = tokenResponse.data.access_token;
+        if (!tokenResponse.data.access_token) {
+        }
+
+        let access_token = tokenResponse.data.access_token;
 
         let userResponse = await axios.post("https://kapi.kakao.com/v2/user/me", null, {
             headers: {
@@ -72,11 +71,9 @@ kakao.get('/', async function (req, res) {
 
     } catch (error) {
         console.error("카카오 로그인 처리 중 오류 발생:", error.response?.data || error.message);
-        res.status(500).json({
-            error: '카카오 로그인 처리 중 오류 발생',
-            details: error.response?.data || error.message
-        });
+        res.status(500).json({ error: '카카오 로그인 처리 중 오류 발생', details: error.response?.data || error.message });
     }
+    
 });
 
 module.exports = kakao;
